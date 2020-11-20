74047d69b025274cbd8e9d0e2257564273bc3db1
zhaoli86 <zhaoli86@gmail.com>
2020-11-20 16:53:21 -0500

    more stuff

---
+++ DeleteOperationForTwoStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeleteOperationForTwoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
