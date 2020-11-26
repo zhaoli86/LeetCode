c00223450ce5064e3602ab96fe260f8f47db097a
zhaoli86 <zhaoli86@gmail.com>
2020-11-25 19:54:26 -0500

    stuff

---
+++ DailyTemperatures/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DailyTemperatures
+{
+    public class Solution
+    {
+        public int[] DailyTemperatures(int[] T)
+        {
+            var stack = new Stack<int>();
+            var result = new int[T.Length];
+            for (int i = 0; i < T.Length; i++)
+            {
+                while (stack.Count!=0 &&  T[stack.Peek()] < T[i])
+                {
+                    var idx = stack.Pop();
+                    result[idx] = i - idx;
+                }
+                stack.Push(i);
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
