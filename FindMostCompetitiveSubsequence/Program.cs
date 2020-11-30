05924dc36bb0f9f141f30a79d64f6a17b48e9a32
zhaoli86 <zhaoli86@gmail.com>
2020-11-29 20:51:50 -0500

    stuff

---
+++ FindMostCompetitiveSubsequence/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindMostCompetitiveSubsequence
+{
+    public class Solution
+    {
+        public int[] MostCompetitive(int[] nums, int k)
+        {
+            var result = new int[k];
+            var stack = new Stack<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (stack.Count!=0 && nums[i] < nums[stack.Peek()] && nums.Length - i + stack.Count > k)
+                {
+                    stack.Pop();
+                }
+                if (result.Length < k)
+                {
+                    stack.Push(i);
+                }
+            }
+
+            for (int i = k - 1; i >= 0; i--)
+            {
+                result[i] = nums[stack.Pop()];
+            }
+
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
