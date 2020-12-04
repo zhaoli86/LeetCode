c3821997c58ed0ca08f9f23d4a8ca79b03227a6b
zhaoli86 <zhaoli86@gmail.com>
2020-12-03 19:35:12 -0500

    stuff

---
+++ RemoveKDigits/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoveKDigits
+{
+    public class Solution
+    {
+        public string RemoveKdigits(string num, int k)
+        {
+            if (k==num.Length)
+            {
+                return "0";
+            }
+            var stack = new Stack<char>();
+            for (int i = 0; i < num.Length; i++)
+            {
+                while (k>0 && stack.Count!=0 && stack.Peek()>num[i])
+                {
+                    stack.Pop();
+                    k--;
+                }
+                stack.Push(num[i]);
+            }
+            while (k>0)
+            {
+                stack.Pop();
+                k--;
+            }
+
+            var sb = new StringBuilder();
+            while (stack.Count!=0)
+            {
+                sb.Insert(0, stack.Pop());
+            }
+
+            while (sb.Length>1 && sb[0]=='0')
+            {
+                sb.Remove(0, 1);
+            }
+            return sb.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
