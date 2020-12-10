5092f3e86663680c4ced9976cae25bad57d09582
zhaoli86 <zhaoli86@gmail.com>
2020-12-09 21:10:32 -0500

    stuff

---
+++ BasicCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicCalculator
+{
+    public class Solution
+    {
+        public int Calculate(string s)
+        {
+            var stack = new Stack<int>();
+            int result = 0;
+            int number = 0;
+            int sign = 1;
+            foreach (var c in s)
+            {
+                if (char.IsDigit(c))
+                {
+                    number = number * 10 + (c - '0');
+                }
+                else if (c=='+')
+                {
+                    result = result + sign*number;
+                    sign = 1;
+                    number = 0;
+                }
+                else if (c=='-')
+                {
+                    result = result + sign * number;
+                    sign = -1;
+                    number = 0;
+                }
+                else if (c=='(')
+                {
+                    stack.Push(result);
+                    stack.Push(sign);
+                    result = 0;
+                    sign = 1;
+                }
+                else if (c==')')
+                {
+                    result = result + sign * number;
+                    result = result * stack.Pop();
+                    result = result + stack.Pop();
+                    number = 0;
+                }
+            }
+            if (number!=0)
+            {
+                result = result + sign * number;
+            }
+
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BasicCalculatorII/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicCalculatorII
+{
+    public class Solution
+    {
+        public int Calculate(string s)
+        {
+            int result = 0;
+            var stack = new Stack<int>();
+            int number = 0;
+            char sign = '+';
+            for (int i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (char.IsDigit(c))
+                {
+                    number = number * 10 + (c - '0');
+                }
+                if ((!char.IsDigit(c) && c!=' ') || i==s.Length-1)
+                {
+                    if (sign=='+')
+                    {
+                        stack.Push(number);
+                    }
+                    else if (sign=='-')
+                    {
+                        stack.Push(-number);
+                    }
+                    else if (sign=='*')
+                    {
+                        stack.Push(stack.Pop() * number);
+                    }
+                    else if (sign=='/')
+                    {
+                        stack.Push(stack.Pop() / number);
+                    }
+
+                    number = 0;
+                    sign = c;
+                }
+            }
+
+            foreach (var item in stack)
+            {
+                result = result + item;
+            }
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
