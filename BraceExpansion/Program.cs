4c20a183aa3b967ee37bb21345b6552dfcd40d0f
zhaoli86 <zhaoli86@gmail.com>
2020-12-13 23:49:05 -0500

    stuff

---
+++ BraceExpansion/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BraceExpansion
+{
+    public class Solution
+    {
+        public string[] Expand(string s)
+        {
+            if (s.Length==0)
+            {
+                return new string[] { "" };
+            }
+            else if (s.Length==1)
+            {
+                return new string[] { s };
+            }
+            var set = new SortedSet<string>();
+
+            if (s[0]=='{')
+            {
+                int i;
+                for (i = 1; s[i] != '}'; i++){}
+                var content = s.Substring(1, i - 1);
+                var strs = content.Split(',');
+                var rest = Expand(s.Substring(i + 1));
+                foreach (var str in strs)
+                {
+                    foreach (var item in rest)
+                    {
+                        set.Add(str + item);
+                    }
+                }
+
+            }
+            else
+            {
+                var rest = Expand(s.Substring(1));
+                foreach (var item in rest)
+                {
+                    set.Add(s[0] + item);
+                }
+            }
+
+            return set.ToArray();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BraceExpansionII/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BraceExpansionII
+{
+    public class Solution
+    {
+        private Queue<string> q = new Queue<string>();
+        private List<string> result = new List<string>();
+        private StringBuilder sb = new StringBuilder();
+        private HashSet<string> set = new HashSet<string>();
+
+        public IList<string> BraceExpansionII(string expression)
+        {
+            q.Enqueue(expression);
+            BraceExImpl();
+            return result.Distinct().OrderBy(a => a).ToList();
+        }
+
+        private void BraceExImpl()
+        {
+            while (q.Count!=0)
+            {
+                var str = q.Dequeue();
+                if (!str.Contains('{'))
+                {
+                    if (!set.Contains(str))
+                    {
+                        set.Add(str);
+                        result.Add(str);
+                    }
+                    continue;
+                }
+
+                int left = 0, right = 0, i = 0;
+
+                while (str[i]!='}')
+                {
+                    if (str[i]=='{')
+                    {
+                        left = i;
+                    }
+                    i++;
+                }
+                right = i;
+
+                var pre = str.Substring(0, left - 0);
+                var post = str.Substring(right + 1);
+                var args = str.Substring(left + 1, right - left - 1).Split(',');
+
+                foreach (var arg in args)
+                {
+                    sb.Clear();
+                    q.Enqueue(sb.Append(pre).Append(arg).Append(post).ToString());
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var input = "{{a,z},a{b,c},{ab,z}}";
+            var result = s.BraceExpansionII(input);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
