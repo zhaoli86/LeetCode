0ea9f3537207cc6039c56764b761b47d1d94f445
zhaoli86 <zhaoli86@gmail.com>
2020-12-16 23:46:28 -0500

    stuff

---
+++ MinimumSizeSubarraySum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MinimumSizeSubarraySum
+{
+    public class Solution
+    {
+        //[2,3,1,2,4,3]
+
+        public int MinSubArrayLen(int s, int[] nums)
+        {
+            if (nums==null || nums.Length==0)
+            {
+                return 0;
+            }
+
+            int i = 0, j = 0, sum = 0, min = int.MaxValue;
+            while (j<nums.Length)
+            {
+                sum += nums[j++];
+                while (sum>=s)
+                {
+                    min = Math.Min(j - i, min);
+                    sum -= nums[i++];
+                }
+            }
+            return min == int.MaxValue ? 0 : min;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
