1086e5124fda45b85c7d33ccf12bb96d8a82bd7e
zhaoli86 <zhaoli86@gmail.com>
2020-12-23 23:52:55 -0500

    stuff

---
+++ FindLongestSubstringContainingEvenVowels/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FindLongestSubstringContainingEvenVowels
+{
+    public class Solution
+    {
+        private int[] c = new int[] { 1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0 };
+
+        public int FindTheLongestSubstring(string s)
+        {
+            int res = 0, mask = 0;
+            var m = new int[32];
+            Array.Fill(m, -1);
+            for (int i = 0; i < s.Length; i++)
+            {
+                //eleetminicoworoep
+                mask ^= c[s[i] - 'a'];
+                if (mask != 0 && m[mask] == -1)
+                {
+                    m[mask] = i;
+                }
+                res = Math.Max(res, i - m[mask]);
+            }
+
+
+
+
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var input = "eleetminicoworoep";
+            var res = s.FindTheLongestSubstring(input);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MinOpsToReduceXtoZero/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace MinOpsToReduceXtoZero
+{
+    public class Solution
+    {
+        public int MinOperations(int[] nums, int x)
+        {
+            int res = int.MaxValue;
+            int left = 0, cur = nums.Sum(), n = nums.Length, right = 0;
+            for (right = 0; right < n; right++)
+            {
+                cur -= nums[right];
+
+                while (cur < x && left <= right)
+                {
+                    cur += nums[left++];
+                }
+                if (cur == x)
+                {
+                    res = Math.Min(res, n - 1 - right + left);
+                }
+            }
+            return res == int.MaxValue ? -1 : res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1<<1>>1);
+            Console.WriteLine(1^2);
+            int cur = 1;
+            cur ^= 1 << 1;
+
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
