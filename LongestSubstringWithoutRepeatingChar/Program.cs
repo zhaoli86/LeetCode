55132aa3d92484de576facacb3a6c6c83c91ff96
zhaoli86 <zhaoli86@gmail.com>
2020-12-24 01:00:40 -0500

    stuff

---
+++ LongestSubstringWithoutRepeatingChar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSubstringWithoutRepeatingChar
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int i = 0, j = 0, res = 0;
+            var set = new HashSet<char>();
+            while (j<s.Length && i<s.Length)
+            {
+                //pwwkew
+                if (!set.Contains(s[j]))
+                {
+                    set.Add(s[j++]);
+                    res = Math.Max(res, j - i);
+                }
+                else
+                {
+                    set.Remove(s[i++]);
+                }
+            }
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "pwwkew";
+            var s = new Solution();
+            var res = s.LengthOfLongestSubstring(input);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
