85d09528d6123dea9625ec33b1c6e9ceb16848f7
zhaoli86 <zhaoli86@gmail.com>
2020-12-25 17:55:07 -0500

    stuff

---
+++ LongestSubstringWithAtMost2DistinctChar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSubstringWithAtMost2DistinctChar
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstringTwoDistinct(string s)
+        {
+            int res = 0, i = 0, j = 0, distinctCount = 0;
+            var map = new Dictionary<char, int>();
+            while (j<s.Length)
+            {
+                //abcabcabc
+                if (map.ContainsKey(s[j]))
+                {
+                    map[s[j]]++;
+                }
+                else
+                {
+                    map[s[j]] = 1;
+                }
+                if (map[s[j]]==1)
+                {
+                    distinctCount++;
+                }
+                j++;
+                while (distinctCount > 2)
+                {
+                    map[s[i]]--;
+                    if (map[s[i]]==0)
+                    {
+                        distinctCount--;
+                    }
+                    i++;
+                }
+                res = Math.Max(res, j - i);
+            }
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var input = "abcabcabc";
+            var res = s.LengthOfLongestSubstringTwoDistinct(input);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LongestSubstringWithAtMostKDistinctChar/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSubstringWithAtMostKDistinctChar
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstringKDistinct(string s, int k)
+        {
+            int res = 0, i = 0, j = 0, distinctCount = 0;
+            var map = new Dictionary<char, int>();
+            while (j < s.Length)
+            {
+                //abcabcabc
+                if (map.ContainsKey(s[j]))
+                {
+                    map[s[j]]++;
+                }
+                else
+                {
+                    map[s[j]] = 1;
+                }
+                if (map[s[j]] == 1)
+                {
+                    distinctCount++;
+                }
+                j++;
+                while (distinctCount > k)
+                {
+                    map[s[i]]--;
+                    if (map[s[i]] == 0)
+                    {
+                        distinctCount--;
+                    }
+                    i++;
+                }
+                res = Math.Max(res, j - i);
+            }
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
