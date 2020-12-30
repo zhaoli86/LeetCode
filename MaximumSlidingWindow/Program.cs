ae70b089c89e4fc5641145b8a8279c7569ddf8c7
zhaoli86 <zhaoli86@gmail.com>
2020-12-30 00:07:56 -0500

    stuff

---
+++ MaximumSlidingWindow/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumSlidingWindow
+{
+    public class Solution
+    {
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return new int[0];
+            }
+            var result = new int[nums.Length + 1 - k];
+
+            var q = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //1, 3, -1, -3, 5, 3, 6, 7
+                if (q.FirstOrDefault() == i - k)
+                {
+                    q.RemoveAt(0);
+                }
+                while (q.Any() && nums[q.Last()] < nums[i])
+                {
+                    q.RemoveAt(q.Count - 1);
+                }
+                q.Add(i);
+                if (i + 1 >= k)
+                {
+                    result[i + 1 - k] = nums[q.First()];
+                }
+            }
+
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 1, 3, -1, -3, 5, 3, 6, 7 };
+            var res = new Solution().MaxSlidingWindow(input, 3);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ShortestSubarrayWithSumAtLeastK/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShortestSubarrayWithSumAtLeastK
+{
+    public class Solution
+    {
+        public int ShortestSubarray(int[] a, int k)
+        {
+            int n = a.Length;
+            int res = n + 1;
+            var list = new List<int>();
+            var prefixSum = new long[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                prefixSum[i + 1] = prefixSum[i] + a[i];
+            }
+            for (int i = 0; i < prefixSum.Length; i++)
+            {
+                while (list.Any() && prefixSum[i] <= prefixSum[list.Last()])
+                {
+                    list.RemoveAt(list.Count - 1);
+                }
+
+                while (list.Any() && prefixSum[i] >= prefixSum[list.First()] + k)
+                {
+                    res = Math.Min(res, i - list.First());
+
+                    list.RemoveAt(0);
+                }
+
+                list.Add(i);
+            }
+            return res == n + 1 ? -1 : res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
