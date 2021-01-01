69e9e92d4d607c6e3aaf5e6bd2c3438be1510b6c
zhaoli86 <zhaoli86@gmail.com>
2021-01-01 00:16:58 -0500

    stuff

---
+++ FindLatestGroupOfSizeM/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FindLatestGroupOfSizeM
+{
+    public class Solution
+    {
+        public int FindLatestStep(int[] arr, int m)
+        {
+            int n = arr.Length, res = -1;
+            var len = new int[n + 2];
+            var count = new int[n + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+                int a = arr[i], left = len[a - 1], right = len[a + 1];
+                len[a] = len[a - left] = len[a + right] = left + right + 1;
+                count[left]--;
+                count[right]--;
+                count[len[a]]++;
+                if (count[m]>0)
+                {
+                    res = i + 1;
+                }
+            }
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ JumpGameVI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JumpGameVI
+{
+    public class Solution
+    {
+        public int MaxResult(int[] nums, int k)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int max = list.Any() ? nums[list.First()] : 0;
+                nums[i] = max + nums[i];
+                while (list.Any() && nums[i] > nums[list.Last()])
+                {
+                    list.RemoveAt(list.Count - 1);
+                }
+                list.Add(i);
+                while (list.Any() && i-list.First()+1>k)
+                {
+                    list.RemoveAt(0);
+                }
+                
+            }
+            return nums[nums.Length-1];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
