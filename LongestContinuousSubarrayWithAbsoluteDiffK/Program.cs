d1935030a442b1b7a98a9387dbe678424a156bc3
zhaoli86 <zhaoli86@gmail.com>
2021-01-01 21:33:24 -0500

    stuff

---
+++ LongestContinuousSubarrayWithAbsoluteDiffK/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace LongestContinuousSubarrayWithAbsoluteDiffK
+{
+    public class Solution
+    {
+        public int LongestSubarray(int[] nums, int limit)
+        {
+            int i = 0, j;
+            var map = new SortedDictionary<int, int>();
+            for (j = 0; j < nums.Length; j++)
+            {
+                map[nums[j]] = map.ContainsKey(nums[j]) ? map[nums[j]] + 1 : 1;
+                if (map.Last().Key - map.First().Key > limit)
+                {
+                    map[nums[i]]--;
+                    if (map[nums[i]] == 0)
+                    {
+                        map.Remove(nums[i]);
+                    }
+                    i++;
+                }
+            }
+            return j - i; 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 10, 1, 2, 4, 7, 2 };
+            int limit = 5;
+
+            var result = new Solution().LongestSubarray(input, limit);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MaxValueOfEquation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaxValueOfEquation
+{
+    public class Solution
+    {
+        public int FindMaxValueOfEquation(int[][] points, int k)
+        {
+            int res = int.MinValue;
+            var list = new List<Tuple<int, int>>();
+            foreach (var point in points)
+            {
+                while (list.Any() && point[0] - list.First().Item2 > k)
+                {
+                    list.RemoveAt(0);
+                }
+                if (list.Any())
+                {
+                    res = Math.Max(res, list.First().Item1 + point[0] + point[1]);
+                }
+                while (list.Any() && point[1]-point[0] > list.Last().Item1)
+                {
+                    list.RemoveAt(list.Count - 1);
+                }
+                list.Add(Tuple.Create(point[1] - point[0], point[0]));
+            }
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
