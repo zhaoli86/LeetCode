fa6bf56ece50d3b7a66a6643250ac143c6e6dafe
zhaoli86 <zhaoli86@gmail.com>
2021-01-15 23:00:03 -0500

    stuff

---
+++ CheapestFlightsWithinKStops/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheapestFlightsWithinKStops
+{
+    public class Solution
+    {
+        private struct Key
+        {
+            public int Src { get; set; }
+            public int Stops { get; set; }
+
+        }
+        private Dictionary<Key, long> map = new Dictionary<Key, long>();
+
+        public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int K)
+        {
+            var adjMatrix = new int[n][];
+            for (int i = 0; i < adjMatrix.Length; i++)
+            {
+                adjMatrix[i] = new int[n];
+            }
+            foreach (var flight in flights)
+            {
+                adjMatrix[flight[0]][flight[1]] = flight[2];
+            }
+
+            long result = GetShortest(adjMatrix, src, dst, K, n);
+
+            if (result >= int.MaxValue)
+            {
+                return -1;
+            }
+            return (int)result;
+        }
+
+        private long GetShortest(int[][] adjMatrix, int src, int dst, int stops, int n)
+        {
+
+            if (src==dst)
+            {
+                return 0;
+            }
+
+            if (stops < 0)
+            {
+                return int.MaxValue;
+            }
+
+            var key = new Key { Src = src, Stops = stops };
+            if (map.ContainsKey(key))
+            {
+                return (int)map[key];
+            }
+            long result = int.MaxValue;
+
+            for (int neighbor = 0; neighbor < n; neighbor++)
+            {
+                int weight = adjMatrix[src][neighbor];
+                if (weight>0)
+                {
+                    result = Math.Min(result, GetShortest(adjMatrix, neighbor, dst, stops - 1, n)+weight);
+                }
+            }
+            map[key] = result;
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[3][] { new int[] { 0, 1, 100 }, new int[] { 1, 2, 100 }, new int[] { 0, 2, 500 } };
+            var result = new Solution().FindCheapestPrice(3, input, 0, 2, 0);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SwimInRisingWater/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SwimInRisingWater
+{
+    public class Solution
+    {
+        public int SwimInWater(int[][] grid)
+        {
+            int lo = grid[0][0];
+            int n = grid.Length;
+            int hi = n * n;
+
+            while (lo<hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (IsPossible(mid, grid))
+                {
+                    hi = mid;
+                }
+                else
+                {
+                    lo = mid + 1;
+                }
+            }
+            return lo;
+        }
+
+        private bool IsPossible(int t, int[][] grid)
+        {
+            var rowShift = new int[] { -1, 1, 0, 0 };
+            var columnShift = new int[] { 0, 0, -1, 1 };
+            var n = grid.Length;
+
+            var stack = new Stack<int>();
+            var seen = new HashSet<int>();
+            seen.Add(0);
+            stack.Push(0);
+
+            while (stack.Any())
+            {
+                var node = stack.Pop();
+                int column = node % n;
+                int row = node / n;
+                if (row==n-1&&column==n-1)
+                {
+                    return true;
+                }
+
+                for (int i = 0; i < rowShift.Length; i++)
+                {
+                    int nr = row + rowShift[i];
+                    int nc = column + columnShift[i];
+                    int val = nc + nr * n;
+                    if (nr>=0&& nr<n && nc>=0 && nc<n && !seen.Contains(val) && grid[nr][nc] <= t)
+                    {
+                        seen.Add(val);
+                        stack.Push(val);
+
+                    }
+                }
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
