98655845a3f6d887b3375bfdae77ee83f48d0291
zhaoli86 <zhaoli86@gmail.com>
2021-01-15 23:37:28 -0500

    stuff

---
+++ JumpGameIII/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace JumpGameIII
+{
+    public class Solution
+    {
+        public bool CanReach(int[] arr, int start)
+        {
+            if (start>=0 && start<arr.Length && arr[start]<arr.Length)
+            {
+                int jump = arr[start];
+                arr[start] += arr.Length;
+                return jump == 0 || CanReach(arr, start + jump) || CanReach(arr, start - jump);
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
