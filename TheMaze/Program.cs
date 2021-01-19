4f297f7361ef280e7de1e0a2a7f9f75845a3c90d
zhaoli86 <zhaoli86@gmail.com>
2021-01-18 20:33:23 -0500

    stuff

---
+++ TheMaze/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace TheMaze
+{
+    public class Solution
+    {
+        public bool HasPath(int[][] maze, int[] start, int[] destination)
+        {
+            var visited = new bool[maze.Length, maze[0].Length];
+
+            return Dfs(maze, start, destination, visited);
+        }
+
+        private bool Dfs(int[][] maze, int[] start, int[] destination, bool[,] visited)
+        {
+            if (visited[start[0], start[1]])
+            {
+                return false;
+            }
+            if (start[0]==destination[0]&&start[1]==destination[1])
+            {
+                return true;
+            }
+
+            visited[start[0], start[1]] = true;
+            int left = start[1] - 1, right = start[1] + 1, up = start[0] - 1, down = start[0] + 1;
+            while (left>=0&&maze[start[0]][left]==0)
+            {
+                left--;
+            }
+            if (Dfs(maze, new int[] {start[0], left+1}, destination, visited))
+            {
+                return true;
+            }
+            while (right<maze[0].Length&&maze[start[0]][right]==0)
+            {
+                right++;
+            }
+            if (Dfs(maze, new int[] {start[0], right -1}, destination, visited))
+            {
+                return true;
+            }
+            while (up>=0&&maze[up][start[1]]==0)
+            {
+                up--;
+            }
+            if (Dfs(maze, new int[] { up+1, start[1]}, destination, visited))
+            {
+                return true;
+            }
+            while (down<maze.Length&& maze[down][start[1]]==0)
+            {
+                down++;
+            }
+            if (Dfs(maze, new int[] { down-1, start[1] }, destination, visited))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[5][] { new int[] { 0, 0, 1, 0, 0 }, new int[] { 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 1, 0 }, new int[] { 1, 1, 0, 1, 1 } ,
+            new int[]{ 0, 0, 0, 0, 0 } };
+            var result = new Solution().HasPath(input, new[] { 0, 4 }, new int[] { 4, 4 });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TheMazeII/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheMazeII
+{
+    public class Solution
+    {
+        public int ShortestDistance(int[][] maze, int[] start, int[] destination)
+        {
+            var q = new Queue<int[]>();
+            var distance = new int[maze.Length, maze[0].Length];
+            for (int i = 0; i < distance.GetLength(0); i++)
+            {
+                for (int j = 0; j < distance.GetLength(1); j++)
+                {
+                    distance[i, j] = int.MaxValue;
+                }
+            }
+            distance[start[0], start[1]] = 0;
+            var dirs = new int[4][] { new int[] { -1, 0 }, new int[] { 1, 0 }, new int[] { 0, -1 }, new int[] { 0, 1 } };
+
+            q.Enqueue(start);
+            while (q.Any())
+            {
+                var node = q.Dequeue();
+                foreach (var dir in dirs)
+                {
+                    int row = node[0] + dir[0];
+                    int column = node[1] + dir[1];
+                    int count = 0;
+                    while (row >= 0 && column >= 0 && row < maze.Length && column < maze[0].Length && maze[row][column] == 0)
+                    {
+                        row += dir[0];
+                        column += dir[1];
+                        count++;
+                    }
+                    if (distance[node[0], node[1]] + count < distance[row - dir[0], column - dir[1]])
+                    {
+                        distance[row - dir[0], column - dir[1]] = distance[node[0], node[1]] + count;
+                        q.Enqueue(new int[] { row - dir[0], column - dir[1] });
+                    }
+                }
+            }
+            return distance[destination[0], destination[1]] == int.MaxValue ? -1 : distance[destination[0], destination[1]];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[5][] { new int[] { 0, 0, 1, 0, 0 }, new int[] { 0, 0, 0, 0, 0 },
+                new int[] { 0,0,0,1,0}, new int[] {1,1,0,1,1 }, new int[] { 0, 0, 0, 0, 0 } };
+            var result = new Solution().ShortestDistance(input, new int[] {0, 4 }, new int[] {4,4 });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
