d71107cadf07778a924b1ae268b9a4dee3bdf1d2
zhaoli86 <zhaoli86@gmail.com>
2021-01-20 22:54:13 -0500

    stuff

---
+++ TrappingRainWaterII/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Utility;
+
+namespace TrappingRainWaterII
+{
+    public class Solution
+    {
+        private class Cell
+        {
+            public int Row { get; set; }
+            public int Column { get; set; }
+            public int Height { get; set; }
+            public Cell(int row, int column, int height)
+            {
+                Row = row;
+                Column = column;
+                Height = height;
+            }
+        }
+
+
+        public int TrapRainWater(int[][] heightMap)
+        {
+            if (heightMap == null || heightMap.Length == 0 || heightMap[0].Length == 0)
+            {
+                return 0;
+            }
+
+            var visited = new bool[heightMap.Length, heightMap[0].Length];
+            var q = new PriorityQueue<Cell, int>(cell => cell.Height);
+
+            int rowCount = heightMap.Length;
+            int columnCount = heightMap[0].Length;
+            for (int i = 0; i < rowCount; i++)
+            {
+                visited[i, 0] = true;
+                visited[i, columnCount - 1] = true;
+                q.Enqueue(new Cell(i, 0, heightMap[i][0]));
+                q.Enqueue(new Cell(i, columnCount - 1, heightMap[i][columnCount - 1]));
+            }
+
+            for (int i = 0; i < columnCount; i++)
+            {
+                visited[0, i] = true;
+                visited[rowCount - 1, i] = true;
+                q.Enqueue(new Cell(0, i, heightMap[0][i]));
+                q.Enqueue(new Cell(rowCount - 1, i, heightMap[rowCount - 1][i]));
+            }
+
+            var dirs = new int[4][] { new int[] {-1, 0 }, new int[] { 1, 0 }, new int[] { 0, -1 },
+            new int[]{0, 1} };
+
+            int result = 0;
+            while (q.Any())
+            {
+                var cell = q.Dequeue();
+                foreach (var dir in dirs)
+                {
+                    int row = cell.Row + dir[0];
+                    int column = cell.Column + dir[1];
+                    if (row >= 0 && row < rowCount && column >= 0 && column < columnCount && !visited[row, column])
+                    {
+                        visited[row, column] = true;
+                        if (cell.Height > heightMap[row][column])
+                        {
+                            result += cell.Height - heightMap[row][column];
+                        }
+                        q.Enqueue(new Cell(row, column, Math.Max(heightMap[row][column], cell.Height)));
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[3][] { new int[] { 1, 4, 3, 1, 3, 2 }, new int[] { 3, 2, 1, 3, 2, 4 }, new int[] { 2, 3, 3, 2, 3, 1 } };
+            var result = new Solution().TrapRainWater(input);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Utility/PriorityQueue.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Utility
+{
+    public class PriorityQueue<TElement, TKey>
+    {
+        private SortedDictionary<TKey, Queue<TElement>> dictionary;
+        private Func<TElement, TKey> selector;
+
+
+        public PriorityQueue(Func<TElement, TKey> selector)
+        {
+            this.selector = selector;
+            this.dictionary = new SortedDictionary<TKey, Queue<TElement>>();
+        }
+
+        public PriorityQueue(Func<TElement, TKey> selector, IComparer<TKey> comparer)
+        {
+            this.selector = selector;
+            this.dictionary = new SortedDictionary<TKey, Queue<TElement>>(comparer);
+        }
+
+        public void Enqueue(TElement item)
+        {
+            TKey key = selector(item);
+            Queue<TElement> queue;
+            if (!dictionary.TryGetValue(key, out queue))
+            {
+                queue = new Queue<TElement>();
+                dictionary.Add(key, queue);
+            }
+
+            queue.Enqueue(item);
+        }
+
+        public TElement Dequeue()
+        {
+            if (dictionary.Count == 0)
+                throw new InvalidOperationException();
+
+            var first = dictionary.First();
+            var queue = first.Value;
+            var output = queue.Dequeue();
+            if (queue.Count == 0)
+                dictionary.Remove(first.Key);
+
+            return output;
+        }
+
+        public bool Any()
+        {
+            return dictionary.Any();
+        }
+    }
+
+
+    public class PriorityQueue<T>
+    {
+        private T[] items;
+        private int N;
+        private Func<T, T, int> comparer;
+
+        public PriorityQueue(Func<T, T, int> comparer, int capacity)
+        {
+            items = new T[capacity + 1];
+            this.comparer = comparer;
+        }
+
+        public bool IsEmpty()
+        {
+            return N == 0;
+        }
+
+        public int Size()
+        {
+            return N;
+        }
+
+        public void Insert(T t)
+        {
+            items[++N] = t;
+            Swim(N);
+        }
+
+        public T Del()
+        {
+            T item = items[1];
+            Swap(1, N--);
+            Sink(1);
+
+            return item;
+        }
+
+        public T Top()
+        {
+            return items[1];
+        }
+
+        public void Remove(T t)
+        {
+            int index = 0;
+
+            for (int i = 1; i < items.Length; i++)
+            {
+                if (comparer(items[i], t) == 0)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            T item = items[index];
+            Swap(index, N--);
+            Sink(index);
+        }
+
+        private void Swim(int k)
+        {
+            while (k > 1 && comparer(items[k / 2], items[k]) > 0)
+            {
+                Swap(k, k / 2);
+                k /= 2;
+            }
+        }
+
+        private void Sink(int k)
+        {
+            while (2 * k <= N)
+            {
+                int j = 2 * k;
+
+                if (j < N && comparer(items[j + 1], items[j]) < 0)
+                {
+                    j++;
+                }
+
+                if (comparer(items[j], items[k]) >= 0)
+                {
+                    break;
+                }
+
+                Swap(k, j);
+                k = j;
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            T temp = items[i];
+            items[i] = items[j];
+            items[j] = temp;
+        }
+    }
+}
