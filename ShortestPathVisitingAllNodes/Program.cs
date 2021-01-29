e2c38cdb20ca30575f3e839b256cfeeb375ea9ab
zhaoli86 <zhaoli86@gmail.com>
2021-01-29 00:17:48 -0500

    stuff

---
+++ ShortestPathVisitingAllNodes/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShortestPathVisitingAllNodes
+{
+    public class Solution
+    {
+        private class State
+        {
+            public int BitMask { get; set; }
+            public int Cur { get; set; }
+            public int Cost { get; set; }
+            public State(int bit, int n, int c)
+            {
+                BitMask = bit;
+                Cur = n;
+                Cost = c;
+            }
+
+            public override bool Equals(object obj)
+            {
+                var p = (State)obj;
+                return p.BitMask == BitMask && p.Cur == Cur;
+            }
+
+            public override int GetHashCode()
+            {
+                return BitMask.GetHashCode() ^ Cur.GetHashCode();
+            }
+        }
+
+        public int ShortestPathLength(int[][] graph)
+        {
+            int n = graph.Length;
+            var q = new Queue<State>();
+            var visited = new HashSet<State>();
+            for (int i = 0; i < n; i++)
+            {
+                int nodeBit = (1 << i);
+                q.Enqueue(new State(nodeBit, i, 0));
+                visited.Add(new State(nodeBit, i, 0));
+            }
+
+            while (q.Any())
+            {
+                var node = q.Dequeue();
+                if (node.BitMask==(1<<n)-1)
+                {
+                    return node.Cost;
+                }
+                var neighbors = graph[node.Cur];
+                foreach (var neighbor in neighbors)
+                {
+                    var bitMask = node.BitMask | (1 << neighbor);
+                    var state = new State(bitMask, neighbor, node.Cost + 1);
+                    if (!visited.Contains(state))
+                    {
+                        q.Enqueue(state);
+                        visited.Add(state);
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[4][] { new int[] { 1, 2 }, new int[] {0, 3 }, new int[] {0, 3 }, new int[] { 1, 2} };
+            var result = new Solution().ShortestPathLength(input);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SnakesAndLadders/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnakesAndLadders
+{
+    public class Solution
+    {
+        public int SnakesAndLadders(int[][] board)
+        {
+            int n = board.Length;
+            var q = new Queue<int>();
+            q.Enqueue(1);
+            var steps = new Dictionary<int, int>();
+            steps[1] = 0;
+            while (q.Any())
+            {
+                int node = q.Dequeue();
+                if (node == n * n)
+                {
+                    return steps[node];
+                }
+                for (int next = node + 1; next <= Math.Min(node + 6, n * n); next++)
+                {
+                    var coordinates = GetCoordiates(next, n);
+                    int x = coordinates.Item1;
+                    int y = coordinates.Item2;
+                    int final = board[x][y] == -1 ? next : board[x][y];
+                    if (!steps.ContainsKey(final))
+                    {
+                        steps[final] = steps[node] + 1;
+                        q.Enqueue(final);
+                    }
+                    
+                }
+            }
+            return -1;
+        }
+
+        private Tuple<int, int> GetCoordiates(int s, int n)
+        {
+            int r = (s-1) / n;
+            int x = n - 1 - r;
+            int y = r % 2 == 0 ? s - 1 - r * n : n + r * n - s;
+            return Tuple.Create(x, y);            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
