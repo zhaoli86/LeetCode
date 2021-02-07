e2be760256cc3a079ebc35a82b8406139bca5cb0
zhaoli86 <zhaoli86@gmail.com>
2021-02-07 16:16:25 -0500

    stuff

---
+++ LetterCombinationsOfPhoneNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LetterCombinationsOfPhoneNumber
+{
+    public class Solution
+    {
+        public IList<string> LetterCombinations(string digits)
+        {
+            if (digits==null || digits.Length==0)
+            {
+                return new List<string>();
+            }
+            var mapping = new string[] { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+            var q = new Queue<string>();
+            q.Enqueue("");
+            for (int i = 0; i < digits.Length; i++)
+            {
+                var digit = int.Parse(digits[i].ToString());
+                while (q.Peek().Length==i)
+                {
+                    var node = q.Dequeue();
+                    foreach (var ch in mapping[digit])
+                    {
+                        q.Enqueue(node + ch);
+                    }
+                }
+            }
+            return q.ToList();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PermutationsII/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PermutationsII
+{
+    public class Solution
+    {
+        public IList<IList<int>> PermuteUnique(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            Array.Sort(nums);
+            Backtrack(result, new List<int>(), new bool[nums.Length], nums);
+            return result;
+        }
+
+        private void Backtrack(List<IList<int>> result, List<int> tempList, bool[] used, int[] nums)
+        {
+            if (tempList.Count == nums.Length)
+            {
+                result.Add(new List<int>(tempList));
+            }
+            else
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (used[i] || i > 0 && nums[i] == nums[i - 1]&&!used[i - 1] )
+                    {
+                        continue;
+                    }
+                    used[i] = true;
+                    tempList.Add(nums[i]);
+                    Backtrack(result, tempList, used, nums);
+                    used[i] = false;
+                    tempList.RemoveAt(tempList.Count - 1);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
