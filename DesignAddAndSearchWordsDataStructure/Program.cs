5c1f819df987e3f98ca7dba1684f96cc4c289098
zhaoli86 <zhaoli86@gmail.com>
2021-02-26 23:27:13 -0500

    stuf

---
+++ DesignAddAndSearchWordsDataStructure/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace DesignAddAndSearchWordsDataStructure
+{
+
+    public class WordDictionary
+    {
+        private class TrieNode
+        {
+            public bool IsWord { get; set; }
+            public TrieNode[] Next { get; set; } = new TrieNode[26];
+        }
+
+        private TrieNode root = new TrieNode();
+
+        public void AddWord(string word)
+        {
+            var p = root;
+
+            foreach (var ch in word)
+            {
+                int idx = ch - 'a';
+                if (p.Next[idx] == null)
+                {
+                    p.Next[idx] = new TrieNode();
+                }
+                p = p.Next[idx];
+            }
+
+            p.IsWord = true;
+        }
+
+        public bool Search(string word)
+        {
+            return Match(word, 0, root);
+        }
+
+        private bool Match(string word, int pos, TrieNode node)
+        {
+            if (pos == word.Length)
+            {
+                return node.IsWord;
+            }
+            if (word[pos] == '.')
+            {
+                for (int i = 0; i < 26; i++)
+                {
+                    if (node.Next[i] != null && Match(word, pos + 1, node.Next[i]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                return node.Next[word[pos] - 'a'] != null && Match(word, pos + 1, node.Next[word[pos] - 'a']);
+            }
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PrefixAndSuffixSearch/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace PrefixAndSuffixSearch
+{
+    public class WordFilter
+    {
+        private class TrieNode
+        {
+            public TrieNode[] Next { get; set; } = new TrieNode[27];
+            public int Weight { get; set; }
+
+            public void Insert(string word, int weight)
+            {
+                var cur = this;
+                foreach (var ch in word)
+                {
+                    if (cur.Next[ch - 'a'] == null)
+                    {
+                        cur.Next[ch - 'a'] = new TrieNode();
+                    }
+                    cur = cur.Next[ch - 'a'];
+                    cur.Weight = weight;
+                }
+            }
+
+            public int StartsWith(string s)
+            {
+                var p = this;
+                foreach (var ch in s)
+                {
+                    if (p.Next[ch - 'a'] == null)
+                    {
+                        return -1;
+                    }
+                    p = p.Next[ch - 'a'];
+                }
+                return p.Weight;
+            }
+        }
+
+
+        private readonly TrieNode root = new TrieNode();
+        public WordFilter(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+
+                for (int j = 0; j <= word.Length; j++)
+                {
+                    root.Insert(word.Substring(j) + "{" + word, i);
+                }
+            }
+        }
+
+        public int F(string prefix, string suffix)
+        {
+            return root.StartsWith(suffix + "{" + prefix);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
