b6006a85c98e718f496ed0005268459efbaeac9a
zhaoli86 <zhaoli86@gmail.com>
2021-03-07 16:39:43 -0500

    stuff

---
+++ LowestCommonAncestorOfBinaryTreeIII/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LowestCommonAncestorOfBinaryTreeIII
+{
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    public class Solution
+    {
+        public Node LowestCommonAncestor(Node p, Node q)
+        {
+            Node a = p, b = q;
+            while (a!=b)
+            {
+                a = a == null ? q : a.parent;
+                b = b == null ? p : b.parent;
+            }
+            return a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LowestCommonAncestorOfBinaryTreeIV/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LowestCommonAncestorOfBinaryTreeIV
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    public class Solution
+    {
+        private TreeNode lca;
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode[] nodes)
+        {
+            var vals = new HashSet<int>();
+            foreach (var node in nodes)
+            {
+                vals.Add(node.val);
+            }
+            Helper(root, vals);
+            return lca;
+        }
+
+        private int Helper(TreeNode root, HashSet<int> vals)
+        {
+            if (root==null)
+            {
+                return 0;
+            }
+            int leftFound = Helper(root.left, vals);
+            int rightFound = Helper(root.right, vals);
+            int foundCount = leftFound + rightFound;
+            if (vals.Contains(root.val))
+            {
+                foundCount++;
+            }
+            if (lca==null&&foundCount==vals.Count)
+            {
+                lca = root;
+            }
+            return foundCount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
