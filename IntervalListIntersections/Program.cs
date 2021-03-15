8a9a625662bbb1144a63172943b3ad30b7fc179b
zhaoli86 <zhaoli86@gmail.com>
2021-03-14 22:18:25 -0400

    stuff

---
+++ IntervalListIntersections/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntervalListIntersections
+{
+    public class Solution
+    {
+        public int[][] IntervalIntersection(int[][] firstList, int[][] secondList)
+        {
+            int m = firstList.Length;
+            int n = secondList.Length;
+            var result = new List<int[]>();
+            int i = 0, j = 0;
+            while (i < m && j < n)
+            {
+                int start = Math.Max(firstList[i][0], secondList[j][0]);
+                int end = Math.Min(firstList[i][1], secondList[j][1]);
+                if (start <= end)
+                {
+                    result.Add(new int[] { start, end });
+                }
+                if (firstList[i][1] < secondList[j][1])
+                {
+                    i++;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MergeInterval/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeInterval
+{
+    public class Solution
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            int len = intervals.Length;
+            var result = new List<int[]>();
+            intervals = intervals.OrderBy(i => i[0]).ToArray();
+
+            var newInterval = intervals[0];
+            result.Add(newInterval);
+            foreach (var interval in intervals)
+            {
+                if (interval[0] <= newInterval[1])
+                {
+                    newInterval[1] = Math.Max(newInterval[1], interval[1]);
+                }
+                else
+                {
+                    newInterval = interval;
+                    result.Add(newInterval);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
