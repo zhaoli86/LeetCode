2dcc88a408bccb0d97c6e78a3b3582baec0bb84f
zhaoli86 <zhaoli86@gmail.com>
2021-03-16 23:17:41 -0400

    stuff

---
+++ InsertInterval/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsertInterval
+{
+    public class Solution
+    {
+        public int[][] Insert(int[][] intervals, int[] newInterval)
+        {
+            var list = new List<int[]>();
+            int start = newInterval[0], end = newInterval[1], len = intervals.Length, i = 0;
+
+            while (i < len && intervals[i][1] < start)
+            {
+                list.Add(intervals[i++]);
+            }
+
+            while (i < len && intervals[i][0] <= newInterval[1])
+            {
+                start = Math.Min(start, intervals[i][0]);
+                end = Math.Max(end, intervals[i][1]);
+                i++;
+            }
+
+            list.Add(new int[] { start, end });
+
+            while (i < len)
+            {
+                list.Add(intervals[i++]);
+            }
+
+            return list.ToArray();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RemoveInterval/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveInterval
+{
+    public class Solution
+    {
+        public IList<IList<int>> RemoveInterval(int[][] intervals, int[] toBeRemoved)
+        {
+            var result = new List<IList<int>>();
+            foreach (var interval in intervals)
+            {
+                if (interval[1] < toBeRemoved[0] || interval[0] > toBeRemoved[1])
+                {
+                    result.Add(interval);
+                }
+                else
+                {
+                    if (interval[0] < toBeRemoved[0])
+                    {
+                        result.Add(new int[] { interval[0], toBeRemoved[0] });
+                    }
+
+                    if (interval[1] > toBeRemoved[1])
+                    {
+                        result.Add(new int[] { toBeRemoved[1], interval[1] });
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
