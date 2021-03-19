0d379c1701329de4a78adb5376731770d2d7bcab
zhaoli86 <zhaoli86@gmail.com>
2021-03-19 00:27:20 -0400

    stuff

---
+++ NonOverlappingIntervals/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace NonOverlappingIntervals
+{
+    public class Solution
+    {
+        public int EraseOverlapIntervals(int[][] intervals)
+        {
+            if (intervals.Length == 0)
+            {
+                return 0;
+            }
+            var count = 1;
+            intervals = intervals.OrderBy(i => i[1]).ToArray();
+            int end = intervals[0][1];
+
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                if (intervals[i][0] >= end)
+                {
+                    count++;
+                    end = intervals[i][1];
+                }
+            }
+
+            return intervals.Length - count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RemoveCoveredIntervals/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace RemoveCoveredIntervals
+{
+    public class Solution
+    {
+        public int RemoveCoveredIntervals(int[][] intervals)
+        {
+            int left = -1, right = -1, result = 0;
+            intervals = intervals.OrderBy(i => i[0]).ToArray();
+            foreach (var interval in intervals)
+            {
+                if (interval[0] > left && interval[1] > right)
+                {
+                    result++;
+                    left = interval[0];
+                }
+                right = Math.Max(right, interval[1]);
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
