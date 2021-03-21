13beef6308f54113f89c0fc821e46986da4f2750
zhaoli86 <zhaoli86@gmail.com>
2021-03-20 21:27:25 -0400

    stuff

---
+++ MeetingScheduler/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MeetingScheduler
+{
+    public class Solution
+    {
+        public IList<int> MinAvailableDuration(int[][] slots1, int[][] slots2, int duration)
+        {
+            var result = new List<int>();
+            slots1 = slots1.OrderBy(s => s[0]).ToArray();
+            slots2 = slots2.OrderBy(s => s[0]).ToArray();
+            int m = slots1.Length, n = slots2.Length, i = 0, j = 0;
+            while (i < m && j < n)
+            {
+                int start = Math.Max(slots1[i][0], slots2[j][0]);
+                int end = Math.Min(slots1[i][1], slots2[j][1]);
+                if (start + duration <= end)
+                {
+                    return new List<int> { start, start + duration };
+                }
+                else if (slots1[i][1] > slots2[j][1])
+                {
+                    j++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MinimumNumberOfArrowsToBurstBalloons/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumNumberOfArrowsToBurstBalloons
+{
+    public class Solution
+    {
+        public int FindMinArrowShots(int[][] points)
+        {
+            if (points.Length == 0)
+            {
+                return 0;
+            }
+            points = points.OrderBy(p => p[1]).ToArray();
+            int result = 1;
+            int endPos = points[0][1];
+            for (int i = 1; i < points.Length; i++)
+            {
+                if (endPos >= points[i][0])
+                {
+                    continue;
+                }
+                result++;
+                endPos = points[i][1];
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
